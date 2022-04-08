57947892fb28019d4a1bfd1b80b11a7f14d4ffba
venkatesan-Juhomi <venkatesans@juhomi.com>
2022-04-08 08:34:42 +0530

    version change

--- lib/say_hi.rb
+++ lib/say_hi.rb
@@ -3,6 +3,13 @@
 require_relative "say_hi/version"
 require "say_hi/generator"
 
-module RandomGreetingGenerator
-  
+# module RandomGreetingGenerator
+
+
+# end
+module SayHi
+  class WelcomeNote
+    def self.greeting(name)
+      "#{"Hello"}, #{name}"
+  end
 end
